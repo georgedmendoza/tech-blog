076734eca125fd41bea37a3e65f7509a710238a7
georgedmendoza <georgedmendoza13@gmail.com>
2021-09-20 17:30:44 -0500

    all post routes working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', (req,res) => {
             {
                 model: Post,
                 attributes: ['id','title','post_url','created_at']
-            },
+            }
             // {
             //     model: Comment,
             //     attributes: ['id','comment_text','created-at'],
